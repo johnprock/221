7f3fd30b6f0d1b4d80652901afbd75e8570e36e1
johnprock <patrickrock@neo.tamu.edu>
2013-02-19 17:04:17 -0600

    updated timing code, need to format output

--- project2/ElapsedTime.cpp
+++ project2/ElapsedTime.cpp
@@ -4,12 +4,59 @@
 #include<ctime>
 #include<iostream>
 #include"ListStack.h"
+#include<math.h>
+#include<iomanip>
 
 using namespace std;
 
 int main() {
-  
-//-------------------PUSH------------------//
+  int size; // size of stack is set at each loop iteration
+  ListStack<int> stack1 = ListStack<int>();
+
+  cout << "Size     push     pop      size     mySize\n";
+  for(int i=0; i<25; i++) { // measure ten powers of 2
+  size = pow(2,i);
+
+    //time push
+    clock_t startPush = clock();
+    for(int j=0; j<size; j++) {
+      stack1.push(0); // time of push is independent of pushed element  
+    }
+    clock_t endPush = clock();
+
+    //time size 
+    clock_t startSize = clock();
+    for(int j=0; j<size; j++) {
+      stack1.size();   
+    }
+    clock_t endSize = clock();   
+
+    //time mySize
+    clock_t startMySize = clock();
+    for(int j=0; j<size; j++) {
+      stack1.mySize();   
+    }
+    clock_t endMySize = clock();
+
+    //time pop
+    clock_t startPop = clock();
+    for(int j=0; j<size; j++) {
+      stack1.pop();   
+    }
+    clock_t endPop = clock(); 
+
+    //calculate average run times
+    double pushTime     = ( (double)(endPush - startPush)/CLOCKS_PER_SEC ); 
+    double popTime      = ( (double)(endPop - startPop)/CLOCKS_PER_SEC );
+    double sizeTime     = ( (double)(endSize - startSize)/CLOCKS_PER_SEC ); 
+    double mySizeTime   = ( (double)(endMySize - startMySize)/CLOCKS_PER_SEC );
+
+    cout << size <<setw(10)<< pushTime <<setw(10)<< popTime 
+      <<setw(10)<< sizeTime <<setw(10)<< mySizeTime << endl;
+  }
+  return 0;
+}
+/*/-------------------PUSH------------------//
   ListStack<int> stack1 = ListStack<int>();
   clock_t start1 = clock();
   for(int i=0; i<10000000; i++) {
@@ -17,9 +64,8 @@ int main() {
   }
 
   clock_t end1 = clock();
-  cout << "CPU elapsed time in seconds: "
+  cout << "Push clock, CPU elapsed time in seconds: "
     <<(double)(end1 - start1)/CLOCKS_PER_SEC <<endl;
-  cout << "Pop Stack: " << stack1.pop() << '\n';
 
 
 //------------------POP-------------------//
@@ -30,7 +76,7 @@ int main() {
   }
 
   clock_t end2 = clock();
-  cout << "CPU elapsed time in seconds: "
+  cout << "Pop clock, CPU elapsed time in seconds: "
     <<(double)(end2 - start2)/CLOCKS_PER_SEC <<endl;
   cout << "Pop Stack: " << stack2.pop() << '\n';
 
@@ -43,7 +89,7 @@ int main() {
   }
 
   clock_t end3 = clock();
-  cout << "CPU elapsed time in seconds: "
+  cout << "Size clock, CPU elapsed time in seconds: "
     <<(double)(end3 - start3)/CLOCKS_PER_SEC <<endl;
   cout << "Pop Stack: " << stack3.pop() << '\n';
 
@@ -55,12 +101,12 @@ int main() {
   }
 
   clock_t end4 = clock();
-  cout << "CPU elapsed time in seconds: "
+  cout << "mySize clock, CPU elapsed time in seconds: "
     <<(double)(end4 - start4)/CLOCKS_PER_SEC <<endl;
   cout << "Pop Stack: " << stack4.pop() << '\n';
   return 0;
 }
-
+*/
 
 /* A way to improve the precision of measurement is to start
  * timing right after the clock ticks and also measure for many
